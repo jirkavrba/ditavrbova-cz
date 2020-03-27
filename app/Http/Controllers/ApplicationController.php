2bc2dad0c022a19525c80a85bf2d9e6e07b14feb
Jiří Vrba <jiri@vrba.dev>
2020-03-27 21:12:48 +0100

    Redirect on homepage

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,22 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\ProductCategory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
 class ApplicationController extends Controller
 {
-    public function index(): Response
+    public function index(): RedirectResponse
     {
-        $data = [
-            'products' => Product::with(
-                'image',
-                'additionalImages',
-                'category',
-                'type'
-            )->sorted(),
-            'categories' => ProductCategory::with('types')->get(),
-        ];
-
-        return response()->view('application', $data);
+        return redirect()->to('https://ditavrbova.cz');
     }
 }
--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -4,35 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="refresh">
     <title>Application</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    <h1>Výpis kategorií</h1>
-    <ol>
-        @foreach($categories as $category)
-            <li>
-                <h2>{{ $category->name }}</h2>
-                <ol>
-                    @foreach($category->types as $type)
-                        <li>{{ $type->name }}</li>
-                    @endforeach
-                </ol>
-            </li>
-        @endforeach
-    </ol>
-    <h1>Výpis produktů</h1>
-    <ol>
-        @foreach($products as $product)
-            <li>
-                <h2>{{ $product->name }}</h2>
-                <img src="{{ $product->imageUrl }}" style="width: 100px; height: 100px;">
-                @foreach($product->additionalImages as $image)
-                    <img src="{{ $image->url }}" style="width: 60px; height: 60px;">
-                @endforeach
-            </li>
-        @endforeach
-    </ol>
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
